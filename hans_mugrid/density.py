b1e82412debce5cce68535cc19b60644350a8337
Hannes Holey <hannes.holey@unimi.it>
2025-09-03 00:43:26 +0200

    boundary conditions

--- hans_mugrid/density.py
+++ hans_mugrid/density.py
@@ -6,8 +6,14 @@ from hans_mugrid.integrate import predictor_corrector, source
 
 class Solution:
 
-    def __init__(self, fc, disc, prop):
+    def __init__(self, fc, disc, prop, bc):
         self.disc = disc
+
+        for k, v in bc.items():
+            if k in ['x0', 'x1', 'y0', 'y1']:
+                bc[k] = np.array(v)
+        self.bc = bc
+
         self.__field = fc.real_field('solution', (3,))
 
         # Constant fields
@@ -27,17 +33,17 @@ class Solution:
     @property
     def density(self):
         # centerline
-        return self.__field.p[0, :, self.disc['Ny'] // 2]
+        return self.__field.p[0, 1:-1, self.disc['Ny'] // 2]
 
     @property
     def flux_x(self):
         # centerline
-        return self.__field.p[1, :, self.disc['Ny'] // 2]
+        return self.__field.p[1, 1:-1, self.disc['Ny'] // 2]
 
     @property
     def flux_y(self):
         # centerline
-        return self.__field.p[2, :, self.disc['Ny'] // 2]
+        return self.__field.p[2, 1:-1, self.disc['Ny'] // 2]
 
     @property
     def kinetic_energy(self):
@@ -96,11 +102,99 @@ class Solution:
 
     def _communicate_ghost_buffers(self):
 
-        # periodic BCs
-        # x
-        self.__field.p[:, 0, :] = self.__field.p[:, -2, :].copy()
-        self.__field.p[:, -1, :] = self.__field.p[:, 1, :].copy()
+        # x0
+        if np.all(self.bc['x0'] == 'P'):
+            self.__field.p[:, 0, :] = self.__field.p[:, -2, :].copy()
+        else:
+            if np.any(self.bc['x0'] == 'D'):
+                self.__field.p[self.bc['x0'] == 'D', :1, :] = self._get_ghost_cell_values('D', axis=0, direction=-1)
+            if np.any(self.bc['x0'] == 'N'):
+                self.__field.p[self.bc['x0'] == 'N', :1, :] = self._get_ghost_cell_values('N', axis=0, direction=-1)
 
-        # y
-        self.__field.p[:, :, 0] = self.__field.p[:, :, -2].copy()
-        self.__field.p[:, :, -1] = self.__field.p[:, :, 1].copy()
+        # x1
+        if np.all(self.bc['x1'] == 'P'):
+            self.__field.p[:, -1, :] = self.__field.p[:, 1, :].copy()
+        else:
+            if np.any(self.bc['x1'] == 'D'):
+                self.__field.p[self.bc['x1'] == 'D', -1:, :] = self._get_ghost_cell_values('D', axis=0, direction=1)
+            if np.any(self.bc['x1'] == 'N'):
+                self.__field.p[self.bc['x1'] == 'N', -1:, :] = self._get_ghost_cell_values('N', axis=0, direction=1)
+
+        # y0
+        if np.all(self.bc['y0'] == 'P'):
+            self.__field.p[:, :, 0] = self.__field.p[:, :, -2].copy()
+        else:
+            if np.any(self.bc['y0'] == 'D'):
+                self.__field.p[self.bc['y0'] == 'D', :, :1] = self._get_ghost_cell_values('D', axis=1, direction=-1)
+            if np.any(self.bc['y0'] == 'N'):
+                self.__field.p[self.bc['y0'] == 'N', :, :1] = self._get_ghost_cell_values('N', axis=1, direction=-1)
+
+        # y1
+        if np.all(self.bc['y0'] == 'P'):
+            self.__field.p[:, :, -1] = self.__field.p[:, :, 1].copy()
+        else:
+            if np.any(self.bc['y1'] == 'D'):
+                self.__field.p[self.bc['y1'] == 'D', :, -1:] = self._get_ghost_cell_values('D', axis=1, direction=1)
+            if np.any(self.bc['y0'] == 'N'):
+                self.__field.p[self.bc['y1'] == 'N', :, -1:] = self._get_ghost_cell_values('N', axis=1, direction=1)
+
+    def _get_ghost_cell_values(self, bc_type, axis, direction, num_ghost=1):
+        """Computes the ghost cell values for boundary conditions.
+
+        For Dirichlet BCs, the target value is reached at the interface between 
+        the outermost cell within the physical domain and the first ghost cell. 
+
+        Neumann BCs will always be with zero gradient.
+
+        For both type of BCs, two different interpolation schemes are implemented 
+        depending on the number of ghost cells (num_ghost<=2).
+
+
+        Parameters
+        ----------
+        bc_type : str
+            'D' for Dirichlet or 'N' for Neumann
+        axis : int
+            Axis, either 0 for x or 1 for y axis.
+        direction : int
+            Upstream (<0) or downstream (>1) direction.
+        num_ghost : int
+            Number of ghost cells.
+
+        Returns
+        -------
+        np.ndarray
+            Ghost cell values.
+        """
+
+        assert bc_type in ["D", "N"]
+
+        if axis == 0:  # x
+            if direction > 0:  # downstream
+                q_target = self.bc["rhox1"]
+                q_adj = self.__field.p[self.bc["x1"] == bc_type, -(num_ghost + num_ghost):-num_ghost, :]
+            else:  # upstream
+                q_target = self.bc["rhox0"]
+                q_adj = self.__field.p[self.bc["x0"] == bc_type, num_ghost:num_ghost + num_ghost, :]
+
+        elif axis == 1:  # y
+            if direction > 0:  # downstream
+                q_target = self.bc["rhoy1"]
+                q_adj = self.__field.p[self.bc["y1"] == bc_type, :, -(num_ghost + num_ghost):-num_ghost]
+            else:  # upstream
+                q_target = self.bc["rhoy0"]
+                q_adj = self.__field.p[self.bc["y0"] == bc_type, :, num_ghost:num_ghost + num_ghost]
+        else:
+            raise RuntimeError("axis must be either 0 (x) or (y)")
+
+        a1 = 1. / 2.
+        a2 = 0.
+        q1 = q_adj
+        q2 = 0.
+
+        if bc_type == "D":
+            Q = (q_target - a1 * q1 + a2 * q2) / (a1 - a2)
+        else:
+            Q = ((1. - a1) * q1 + a2 * q2) / (a1 - a2)
+
+        return Q
--- main.py
+++ main.py
@@ -17,11 +17,20 @@ if __name__ == "__main__":
 
     disc = {'Lx': 1.e-3,
             'Ly': 1.,
-            'Nx': 100,
-            'Ny': 1,
+            'Nx': 50,
+            'Ny': 20,
             'CR': 1.e-2,
             'eps': 0.7
             }
+
+    bc = {'x0': ['D', 'N', 'N'],
+          'x1': ['D', 'N', 'N'],
+          'y0': ['D', 'N', 'N'],
+          'y1': ['D', 'N', 'N'],
+          'rhox0': prop['rho0'],
+          'rhox1': prop['rho0'],
+          'rhoy0': prop['rho0'],
+          'rhoy1': prop['rho0']}
 
     disc['dx'] = disc['Lx'] / disc['Nx']
     disc['dy'] = disc['Ly'] / disc['Ny']
@@ -36,23 +45,25 @@ if __name__ == "__main__":
 
     # intialize all fields
     geometry = GapHeight(fc, disc)
-    solution = Solution(fc, disc, prop)
+    solution = Solution(fc, disc, prop, bc)
 
     solution.initialize(prop['rho0'], 0.1, 0.)
 
     file = FileIONetCDF('example.nc', OpenMode.Overwrite)
     file.register_field_collection(fc, field_names=['solution', 'pressure'])
 
-    Nt = 2000
+    Nt = 1000
 
     fig, ax = plt.subplots(2, 3, figsize=(12, 6))
 
-    for i in range(Nt):
+    i = 0
+    print(f"{i:>5d} {solution.residual:.3e}")
+    file.append_frame().write()
+
+    for i in range(1, Nt + 1):
         solution.update(i % 2 == 0)
 
         if i % 100 == 0:
-
-            print(f"{i:>5d} {solution.residual:.3e}")
 
             color_q = plt.cm.Blues(i / Nt)
             ax[0, 0].plot(solution.density, color=color_q)
@@ -61,10 +72,11 @@ if __name__ == "__main__":
 
             color_p = plt.cm.Greens(i / Nt)
             color_t = plt.cm.Oranges(i / Nt)
-            ax[1, 0].plot(solution.pressure.pressure, color=color_p)
-            ax[1, 1].plot(solution.wall_stress.lower[4, :, disc['Ny'] // 2], color=color_t)
-            ax[1, 2].plot(solution.wall_stress.upper[4, :, disc['Ny'] // 2], color=color_t)
+            ax[1, 0].plot(solution.pressure.pressure[1:-1, disc['Ny'] // 2], color=color_p)
+            ax[1, 1].plot(solution.wall_stress.lower[4, 1:-1, disc['Ny'] // 2], color=color_t)
+            ax[1, 2].plot(solution.wall_stress.upper[4, 1:-1, disc['Ny'] // 2], color=color_t)
 
+            print(f"{i:>5d} {solution.residual:.3e}")
             file.append_frame().write()
 
     plt.show()
