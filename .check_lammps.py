22b75a7530923cabc2b2a383a0bad6d778c57c9a
Hannes Holey <hannes.holey@unimi.it>
2025-10-20 10:24:35 +0200

    MAINT: check lammps import before printing

--- .check_lammps.py
+++ .check_lammps.py
@@ -1,9 +1,14 @@
-import lammps
+try:
+    from lammps import lammps
+except ImportError:
+    lammps = None
 
-lmp = lammps.lammps(cmdargs=['-log', 'none', "-screen", 'none'])
-
-print('LAMMPS Version: ', lmp.version())
-print('OS:', lmp.get_os_info())
-print('MPI: ', lmp.has_mpi_support)
-print('mpi4py: ', lmp.has_mpi4py)
-print('Installed packages:', lmp.installed_packages)
+if lammps is not None:
+    lmp = lammps(cmdargs=['-log', 'none', "-screen", 'none'])
+    print('LAMMPS Version: ', lmp.version())
+    print('OS:', lmp.get_os_info())
+    print('MPI: ', lmp.has_mpi_support)
+    print('mpi4py: ', lmp.has_mpi4py)
+    print('Installed packages:', lmp.installed_packages)
+else:
+    print('LAMMPS Python module is not installed')
