85f972ce8d886f55b4e92cb2ca64ba28df0f14cd
Hannes Holey <hannes.holey@unimi.it>
2025-11-25 10:21:26 +0100

    DOC: switch doc theme

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@ exclude_patterns = ['conf.py']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'  # 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 
 # -- Extension configuration -------------------------------------------------
 
