cd959d27ad58f95e526ba37925b8f1cd609b3607
Hannes Holey <hannes.holey@unimi.it>
2025-12-02 17:45:27 +0100

    MAINT: split MD runner and class definitions

--- GaPFlow/md/__init__.py
+++ GaPFlow/md/__init__.py
@@ -21,4 +21,4 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
-from .runner import MolecularDynamics, Mock, LennardJones, GoldAlkane  # noqa: F401
+from .md import MolecularDynamics, Mock, LennardJones, GoldAlkane  # noqa: F401
